019ff299e0ec3aea8b4112219c7b3ea544bf0cdb
JosephGabriel <josegabriel.freelancer@gmail.com>
2021-10-26 13:08:58 -0300

    App finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,82 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              child: ClipOval(child: Image.asset('images/joseph.png')),
+            ),
+            Text(
+              "Joseph Gabriel",
+              style: TextStyle(
+                  color: Colors.white, fontSize: 30, fontFamily: 'Pacifico'),
+            ),
+            Text(
+              "Fullstack Developer",
+              style: TextStyle(
+                color: Colors.teal[100],
+                fontSize: 20,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Sans Pro',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text('(16)99248-5068',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18,
+                      )),
+                )),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text('josegabriel.jgs18@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 18,
+                    )),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
